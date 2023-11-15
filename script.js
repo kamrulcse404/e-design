9536ae5fd411023131d93446faee8510fdaaacf6
kamrulcse404 <kamrul.h01960@gmail.com>
2023-11-15 16:25:57 +0600

    first

---
+++ script.js
@@ -0,0 +1,15 @@
+// function copyMenu(){
+//     var dptCategory = document.querySelector('.dpt-cat');
+//     var dptPlace = document.querySelector('.departments');
+//     dptPlace.innerHTML = dptCategory.innerHTML;
+
+//     var mainNav = document.querySelector('.header-nav nav');
+//     var nvPlace = document.querySelector('.off-canvas nav');
+//     nvPlace.innerHTML = mainNav.innerHTML;
+
+//     var topNav = document.querySelector('.header-top .wrapper');
+//     var topPlace = document.querySelector('.off-canvas .thetop-nav');
+//     nvPlace.innerHTML = mainNav.innerHTML;
+// }
+
+// copyMenu();
