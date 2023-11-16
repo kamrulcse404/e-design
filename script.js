78422359a4b163c996a9e4115d8380b62e2b69f9
kamrulcse404 <kamrul.h01960@gmail.com>
2023-11-16 12:47:18 +0600

    mobile menu

--- script.js
+++ script.js
@@ -7,9 +7,33 @@ function copyMenu(){
     var navPlace = document.querySelector('.off-canvas nav');
     navPlace.innerHTML = mainNav.innerHTML;
 
-    var topNav = document.querySelector('.header-top');
+    var topNav = document.querySelector('.header-top .wrapper');
     var topPlace = document.querySelector('.off-canvas .thetop-nav');
     topPlace.innerHTML = topNav.innerHTML;
 }
 
 copyMenu();
+
+
+const menuButton = document.querySelector('.trigger'),
+        closeButton = document.querySelector('.t-close'),
+        addClass = document.querySelector('.site');
+menuButton.addEventListener('click', function(){
+    addClass.classList.toggle('showmenu')
+})
+
+closeButton.addEventListener('click', function(){
+    addClass.classList.remove('showmenu')
+})
+
+
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+
+function toggle(e){
+    e.preventDefault();
+    submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+
+    if (this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+}
