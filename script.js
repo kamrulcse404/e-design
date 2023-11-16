26dbf29dc7eec84a8a44623d67f6624488b7d603
kamrulcse404 <kamrul.h01960@gmail.com>
2023-11-16 11:38:49 +0600

    aside and js added

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// function copyMenu(){
-//     var dptCategory = document.querySelector('.dpt-cat');
-//     var dptPlace = document.querySelector('.departments');
-//     dptPlace.innerHTML = dptCategory.innerHTML;
+function copyMenu(){
+    var dptCategory = document.querySelector('.dpt-cat');
+    var dptPlace = document.querySelector('.departments');
+    dptPlace.innerHTML = dptCategory.innerHTML;
 
-//     var mainNav = document.querySelector('.header-nav nav');
-//     var nvPlace = document.querySelector('.off-canvas nav');
-//     nvPlace.innerHTML = mainNav.innerHTML;
+    var mainNav = document.querySelector('.header-nav nav');
+    var navPlace = document.querySelector('.off-canvas nav');
+    navPlace.innerHTML = mainNav.innerHTML;
 
-//     var topNav = document.querySelector('.header-top .wrapper');
-//     var topPlace = document.querySelector('.off-canvas .thetop-nav');
-//     nvPlace.innerHTML = mainNav.innerHTML;
-// }
+    var topNav = document.querySelector('.header-top');
+    var topPlace = document.querySelector('.off-canvas .thetop-nav');
+    topPlace.innerHTML = topNav.innerHTML;
+}
 
-// copyMenu();
+copyMenu();
